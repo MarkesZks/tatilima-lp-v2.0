945323df55f5fc1284a8fd98ca0711701a1bf1da
Gabriel Marques <gabriel.marquesbiel@hotmail.com>
2024-09-16 21:44:58 -0300

    :feat Header Component

---
+++ src/Main.js
@@ -0,0 +1,11 @@
+
+import '../src/styles/main.scss';
+import Header from './components/Header.jsx';
+
+export default function App(){
+  return(
+    <div className='App'>
+      <Header/> 
+    </div>
+  )
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,37 @@
+import "../styles/components/header.scss";
+import logo from "../assets/images/logoHeader.png";
+import {
+  FaInstagram,
+  FaWhatsapp,
+  FaPinterest,
+  FaSpotify,
+} from "react-icons/fa";
+
+const Header = () => {
+  return (
+    <>
+      <header className="header">
+        <img src={logo} />
+        <div className="navbar">
+          <a href="" target="_blank">
+            <FaInstagram />
+          </a>
+          <a href="" target="_blank">
+            <FaWhatsapp />
+          </a>
+          <a href="" target="_blank">
+            <FaPinterest />
+          </a>
+          <a href="" target="_blank">
+            <FaSpotify />
+          </a>
+        </div>
+      </header>
+      <div className="line-container">
+        <span></span>
+      </div>
+    </>
+  );
+};
+
+export default Header;
