03f8428a5172ad7886bdace692683cd0aac23170
felipematthew <felipematthewnascimento@gmail.com>
2024-09-21 15:15:45 -0300

    Feat: responsive mixins header and main

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
   return (
     <>
       <header className="header">
-        <img src={logo} />
+        <img src={logo} alt/>
         <div className="navbar">
           <a href="" target="_blank">
             <FaInstagram />
--- src/components/MainSectionTati.jsx
+++ src/components/MainSectionTati.jsx
@@ -1,25 +1,28 @@
 import "../styles/components/mainSectionTati.scss";
 import logoTituloMain from "../assets/images/logoMain.png";
 import ProfileMainImg from "../assets/images/ProfileMainImg.png";
+import { ArrowDown } from "lucide-react";
 
 const MainSectionTati = () => {
-  return (
-    <>
-    <section className="container">
-            <img src={ProfileMainImg} className="imgMainProfile" alt="perfil main imagem"/>
-        <div className="textContainer">
-            <h1>Ceo Studio Tati Lima & Mentora</h1>
-            <img src={logoTituloMain}  alt="logo titulo main"/>
-            <div className="scrollDown">
-                <span>Role para baixo</span>
-            </div>
-        </div>
-      
-    </section>
-
-     
-    </>
-  );
+	return (
+		<section className="main-section">
+			<div className="container">
+				<img
+					src={ProfileMainImg}
+					className="imgMainProfile"
+					alt="perfil main imagem"
+				/>
+				<div className="textContainer">
+					<span>Ceo Studio Tati Lima & Mentora</span>
+					<img src={logoTituloMain} alt="logo titulo main" />
+					<div className="scrollDown">
+						<ArrowDown />
+						<span>Role para baixo</span>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default MainSectionTati;
