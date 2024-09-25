2db6348ac716afb57d6c2af25e4d45a70c5ae1d7
felipematthew <felipematthewnascimento@gmail.com>
2024-09-25 20:14:08 -0300

    feat: Courses content json file

--- src/components/Courses.jsx
+++ src/components/Courses.jsx
@@ -1,34 +1,42 @@
-import Card from "./Card"
+import Card from "./Card";
 
-import img from '../../src/assets/images/ProfileMainImg.png'
-import '../styles/components/courses.scss'
-
+import img from "../../src/assets/images/ProfileMainImg.png";
+import "../styles/components/courses.scss";
+import { faceToFaceCourses, onlineCourses } from "../utils/cart_content";
 
 const Courses = () => {
-  return (
-    <div className="courses-container">
-      <div className="online-courses">
-        <h2>Cursos online</h2>
-        <div>
-          <Card title="Curso 1" content="Lorem ipsum lorem ipsum" buttonText="Garanta já" image={img} />
-          <Card title="Curso 1" content="Lorem ipsum lorem ipsum" buttonText="Garanta já" image={img} />
-          <Card title="Curso 1" content="Lorem ipsum lorem ipsum" buttonText="Garanta já" image={img} />
-          <Card title="Curso 1" content="Lorem ipsum lorem ipsum" buttonText="Garanta já" image={img} />
-          <Card title="Curso 1" content="Lorem ipsum lorem ipsum" buttonText="Garanta já" image={img} />
+	return (
+		<div className="courses-container">
+			<div className="face-to-face-courses">
+				<h2>Cursos presenciais</h2>
+				<div>
+          {faceToFaceCourses.map((course, index) => (
+            <Card 
+              key={course.title}
+              title={course.title}
+              content={course.content}
+              buttonText={course.buttonText}
+              image={course.image}
+            />
+          ))}
         </div>
-      </div>
-      <div  className="face-to-face-courses">
-          <h2>Cursos presenciais</h2>
-          <div>
-          <Card title="Curso 1" content="Lorem ipsum lorem ipsum" buttonText="Garanta já" image={img} />
-          <Card title="Curso 1" content="Lorem ipsum lorem ipsum" buttonText="Garanta já" image={img} />
-          <Card title="Curso 1" content="Lorem ipsum lorem ipsum" buttonText="Garanta já" image={img} />
-          <Card title="Curso 1" content="Lorem ipsum lorem ipsum" buttonText="Garanta já" image={img} />
-          <Card title="Curso 1" content="Lorem ipsum lorem ipsum" buttonText="Garanta já" image={img} />
+			</div>
+			<div className="online-courses">
+				<h2>Cursos online</h2>
+				<div>
+          {onlineCourses.map((course, index) => (
+            <Card 
+              key={course.title}
+              title={course.title}
+              content={course.content}
+              buttonText={course.buttonText}
+              image={course.image}
+            />
+          ))}
         </div>
-      </div>
-    </div>
-  )
-}
+			</div>
+		</div>
+	);
+};
 
-export default Courses
+export default Courses;
---
+++ src/utils/cart_content.js
@@ -0,0 +1,42 @@
+import ImageStartBrow from '../assets/images/start-brows.png'
+import ImageEvolutionBrows from '../assets/images/evolution-brows.png'
+import ImagePlanejamentoLuz from '../assets/images/planejamento-luz.png'
+import ImageConsultoriaShine from '../assets/images/consultoria-shine.png'
+import ImageMentoriaQueen from '../assets/images/mentoria-queen.png'
+// TODO: Trabalhar com os breakpoints no content
+
+export const faceToFaceCourses = [
+  {
+    title: "Start Brows",
+    content: "Você terá acesso a técnica que fez a Tati ser eleita a melhor Designer de Sobrancelhas da sua cidade. Ira aprender sobre mapeamento estratégico, epilação na cera, henna, marketing e muito mais! Para ter acesso a todas as informações e aprender presencialmente, clique no botão abaixo",
+    image: ImageStartBrow,
+    buttonText: "Garanta já!"
+  },
+  {
+    title: "Evolution Brows",
+    content: "O curso avançado da Tati Lima, você irá sanar todas suas dúvidas sobre sobrancelhas. Esse curso tem como foco em desenvolver seus dificuldades e te especializar em sobrancelhas. Para ter sua técnica mais descomplicada, diminuir seu tempo e aprender estratégias de vendas, clique no botão abaixo para mais informações sobre o curso presencia.",
+    image: ImageEvolutionBrows,
+    buttonText: "Saiba mais!",
+  }
+]
+
+export const onlineCourses = [
+  {
+    title: "Planejamento Luz 💡",
+    content: "O maior organizador de conteúdo para profissionais da área da beleza! Esse template contém tudo o que você precisa saber para se organizar melhor com seus posts para a divulgação do seu trabalho. Não ficará sem ideias do que postar, será mais constante e irá ter resultados imediatos! Você terá 2 aulas de como usar o template para sua área do Notion e o acesso é vitalício!",
+    image: ImagePlanejamentoLuz,
+    buttonText: "Brilhe no seu Instagram!",
+  },
+  {
+    title: "Consultoria Shine",
+    content: "A Tati irá apresentar um plano de ação pronto para você, profissional da área da beleza, postar conteúdos que irá converter seu seguidores em clientes, postando menos e vendendo mais! Dentro da consultoria, você terá 2 encontros individuais, 1 mês de acompanhamento e o plano de ação com conteúdos prontos para postar, em diferentes formatos para a divulgação atraente do seu serviço beauty!",
+    image: ImageConsultoriaShine,
+    buttonText: "Quero seu direcionamento!",
+  },
+  {
+    title: "Mentoria Queen 👑",
+    content: "Vou mergulhar profundamente na análise do seu perfil, do seu nicho na área da beleza e do público que você deseja atingir. Com base nessa análise minuciosa e na entrevista que realizaremos, vou desenvolver uma estratégia personalizada que se alinha diretamente com suas principais metas para o seu perfil. Tudo isso será feito respeitando sua autenticidade e preservando a essência do seu trabalho. Juntas, vamos governar o seu reino digital, elevando sua presença online e destacando seu papel como um verdadeiro profissional da beleza.",
+    image: ImageMentoriaQueen,
+    buttonText: "Acesse já!",
+  }
+]
